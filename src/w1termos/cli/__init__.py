382e3dae410eec260458c3fa0102ccbf2a1ecbba
cacko <alex@cacko.net>
2025-10-24 06:19:53 +0100

    I hate this fucking language.

--- src/w1termos/cli/__init__.py
+++ src/w1termos/cli/__init__.py
@@ -1,8 +1,8 @@
-
 from typing_extensions import Annotated
 import rich
 import typer
-from w1termos.sensor import get_sensors
+from w1termos.sensor import get_sensors, get_celcius, get_sensor
+import logging
 
 cli = typer.Typer()
 
@@ -12,10 +12,19 @@ def ls():
     for sensor in get_sensors():
         rich.print(sensor)
 
+
 @cli.command()
-def get():
-    pass
-    
+def temp(id: Annotated[str, typer.Argument()] = None):
+    try:
+        if not id:
+            sensor = [sensor for sensor in get_sensors()][0]
+        else:
+            sensor = get_sensor(id)
+        assert sensor
+        rich.print(get_celcius(sensor=sensor))
+    except Exception as e:
+        logging.error(e)
+
 
 @cli.callback()
 def main(ctx: typer.Context):
--- src/w1termos/sensor/__init__.py
+++ src/w1termos/sensor/__init__.py
@@ -3,3 +3,9 @@ from w1thermsensor import W1ThermSensor
 def get_sensors():
     for sensor in W1ThermSensor.get_available_sensors():
        yield sensor
+       
+def get_celcius(sensor: W1ThermSensor):
+    return sensor.get_temperature()
+
+def get_sensor(id: str):
+    return W1ThermSensor(sensor_id=id)
